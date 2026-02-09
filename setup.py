1a68a0f20be82e8927d0fac0ce75974389b52361
kevin-amayi <kevinamayi20@gmail.com>
2026-02-09 20:28:04 +0000

    fix: Add pyproject.toml for Docker uv pip compatibility

--- setup.py
+++ setup.py
@@ -3,21 +3,15 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in imprest_management/__init__.py
 from imprest_management import __version__ as version
 
 setup(
     name="imprest_management",
     version=version,
     description="Complete imprest (cash advance) management system for Frappe/ERPNext",
-    author="Your Company",
-    author_email="info@yourcompany.com",
-    packages=find_packages(
-        include=[
-            "imprest_management",
-            "imprest_management.*",
-        ]
-    ),
+    author="Tagrit",
+    author_email="info@tagrit.com",
+    packages=find_packages(exclude=["*.workspace", "*.workspace.*"]),
     zip_safe=False,
     include_package_data=True,
     install_requires=install_requires
