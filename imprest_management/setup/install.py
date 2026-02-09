125853425acf47372174d94a6a9f8f97900eaaaa
kevin-amayi <kevinamayi20@gmail.com>
2026-02-09 22:59:00 +0000

    fix: Add all required files for proper installation

--- imprest_management/setup/install.py
+++ imprest_management/setup/install.py
@@ -1,8 +1,5 @@
 import frappe
 
 def after_install():
-    """
-    Called after app is installed
-    """
+    """Called after app is installed"""
     frappe.db.commit()
-    print("Imprest Management app installed successfully!")
